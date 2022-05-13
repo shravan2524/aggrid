4cf20c170f2afa950f2aefcb8ca6ddda19695d83
Baghina Radu Adrian <softmixt@gmail.com>
2022-05-13 10:55:33 +0200

    install js-cookie and replace react-helmet with react-helmet-async

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetData } from 'react-helmet-async';
 
 interface PageTitleProps {
   title: string
 }
-
+const helmetData = new HelmetData({});
 export default function PageTitle({ title }:PageTitleProps) {
   return (
-    <Helmet>
-      <title>{title}</title>
+    <Helmet helmetData={helmetData}>
+      <title>
+        Finkraft -
+        {' '}
+        {title}
+      </title>
     </Helmet>
 
   );
