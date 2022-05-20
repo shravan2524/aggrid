467dca899789e982ebf7043ce9fa15c34f6ae97d
Baghina Radu Adrian <softmixt@gmail.com>
2022-05-20 15:56:24 +0200

    Add default testing Dromo front KEY

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,2 +1,2 @@
 export const BACKEND_API = process.env.REACT_APP_BACKEND_API;
-export const DROMO_FRONTEND_API_KEY = process.env.REACT_APP_DROMO_FRONTEND_API_KEY;
+export const DROMO_FRONTEND_API_KEY = process.env.REACT_APP_DROMO_FRONTEND_API_KEY ?? '81ea3a5d-43b6-4080-8e34-a4a19fbfe2b3';
