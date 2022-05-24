574e25ad008fc4e53d1c49760529d5471dbe0546
Baghina Radu Adrian <softmixt@gmail.com>
2022-05-24 10:54:02 +0200

    add QR ag-grid

--- src/pages/customer/reconciliation/qr/ReconciliationQrPage.tsx
+++ src/pages/customer/reconciliation/qr/ReconciliationQrPage.tsx
@@ -1,18 +1,228 @@
-import React, { useEffect } from 'react';
+import React, {
+  useCallback,
+  useMemo, useRef, useState,
+} from 'react';
 import PageTitle from 'components/PageTitle';
 import { fetchQRData } from 'services/qrAPIService';
 
+import { AgGridReact } from 'ag-grid-react';
+import { agGridRowDrag } from 'app/utils/Helpers';
+
 export default function ReconciliationQrPage() {
-  useEffect(() => {
-    fetchQRData().then((qrData) => {
-      console.log(qrData);
+  const gridRef = useRef<any>();
+
+  const [rowData, setRowData] = useState<any>();
+
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      headerName: 'QR Details',
+      children: [
+        {
+          field: 'Id',
+          agGridRowDrag,
+          filter: 'agNumberColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Workspace',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Invoice Number',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Invoice Date',
+          agGridRowDrag,
+          filter: 'agDateColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Seller Gstin',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Buyer Gstin',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+
+        {
+          field: 'Document Type',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+
+        {
+          field: 'Total Amount',
+          agGridRowDrag,
+          filter: 'agNumberColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Irn',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Irn Date',
+          agGridRowDrag,
+          filter: 'agDateColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Items Count',
+          agGridRowDrag,
+          filter: 'agNumberColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Hsn Code',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Nic Sign',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+
+        {
+          field: 'QR Detected',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+
+        // Meta Data
+        {
+          field: 'Filename',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+
+        {
+          field: 'S3 Url',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+
+        {
+          field: 'Page Image Link',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+        // ./Fin - Meta Data
+
+        {
+          field: 'Row Updated At',
+          agGridRowDrag,
+          filter: 'agDateColumnFilter',
+          chartDataType: 'category',
+        },
+        {
+          field: 'Extra Information',
+          agGridRowDrag,
+          filter: 'agTextColumnFilter',
+          chartDataType: 'category',
+        },
+      ],
+    },
+  ]);
+
+  const sideBar = useMemo(() => ({
+    toolPanels: [
+      {
+        id: 'columns',
+        labelDefault: 'Columns',
+        labelKey: 'columns',
+        iconKey: 'columns',
+        toolPanel: 'agColumnsToolPanel',
+      },
+      {
+        id: 'filters',
+        labelDefault: 'Filters',
+        labelKey: 'filters',
+        iconKey: 'filter',
+        toolPanel: 'agFiltersToolPanel',
+      },
+
+    ],
+    defaultToolPanel: 'customStats',
+  }), []);
+
+  const defaultColDef = useMemo(() => ({
+    sortable: true,
+    filter: true,
+    resizable: true,
+    floatingFilter: true,
+    enableRowGroup: true,
+    editable: true,
+    enablePivot: true,
+    enableValue: true,
+  }), []);
+
+  const onFirstDataRendered = useCallback((params) => {
+    //  gridRef.current.api.sizeColumnsToFit();
+  }, []);
+
+  const statusBar = useMemo(() => ({
+    statusPanels: [
+      {
+        statusPanel: 'agAggregationComponent',
+        statusPanelParams: {
+          aggFuncs: ['count', 'sum'],
+        },
+      },
+    ],
+  }), []);
+
+  const onGridReady = useCallback((params) => {
+    fetchQRData().then((twoAData) => {
+      console.log(twoAData);
+      setRowData(twoAData);
     });
   }, []);
 
   return (
-    <div className="container">
-      <PageTitle title="Reconciliation" />
-      <h1>Reconciliation QR Page</h1>
+    <div className="container-fluid ag-theme-alpine grid-container-style">
+      <PageTitle title="QR" />
+      <AgGridReact
+        ref={gridRef}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        sideBar={sideBar}
+        rowSelection="multiple"
+        rowDragManaged
+        rowDragMultiRow
+        rowGroupPanelShow="always"
+        defaultColDef={defaultColDef}
+        enableCharts
+        groupDisplayType="multipleColumns"
+        animateRows
+        onGridReady={onGridReady}
+        pagination
+        onFirstDataRendered={onFirstDataRendered}
+        groupIncludeFooter
+        groupIncludeTotalFooter
+        enableRangeSelection
+        statusBar={statusBar}
+        masterDetail
+      />
     </div>
   );
 }
