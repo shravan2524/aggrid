c6d672ee9f5491912140ceaa62a33ce9f5c4ec17
Dragos Moinescu <dragos@appesco.com>
2022-06-06 22:30:43 +0200

    added "view type" switch

--- src/pages/public/qr-helper/Import.tsx
+++ src/pages/public/qr-helper/Import.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as XLSX from 'xlsx';
 
 import './Import.css';
@@ -45,70 +45,24 @@ function File(props) {
 }
 
 function ImportOld(props) {
+  const { data, fileName, sheetName } = props;
+  const [items, setItems] = useState<any>({});
 
-  const [items, setItems] = useState({});
-  const [sheetNames, setSheetNames] = useState([String]);
-  const [sheetName, setSheetName] = useState('');
-  const [wb, setWb] = useState(null);
-  const [fileName, setFileName] = useState('');
-  const [noFiles, setNoFiles] = useState(10);
-
-  const onFileUploaded = (e) => {
-    const [file] = e.target.files;
-    const reader = new FileReader();
-    setFileName(file.name);
-
-    console.log(file);
-
-    reader.onload = (evt: any) => {
-      const bstr = evt.target.result;
-      const wbX: any = XLSX.read(bstr, { type: "binary" });
-      setWb(wbX);
-      const sheetNames = [...wbX.SheetNames];
-      setSheetNames(sheetNames);
-      // const wsname = wb.SheetNames[0];
-      // const ws = wb.Sheets[wsname];
-      // const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
-      // console.log(data);
-    };
-    reader.readAsBinaryString(file);
-  }
-
-  const onSheetNameSelected = (e) => {
-    const sheetNameX = e.target.value;
-    setSheetName(sheetNameX);
-    const wbX: any = wb;
-    const ws = wbX.Sheets[sheetNameX];
-    const data = XLSX.utils.sheet_to_json(ws, { header: 1 });// skip header
-    // then sort the data
-    data.sort((a: any, b: any) => {
-      if (a[0] === b[0]) {
-        if (a[2] < b[2]) {
-          return -1;
-        }
-
-        return 1;
-      }
-
-      if (a[0] < b[0]) {
-        return -1;
-      }
-
-      return 1;
-    });
-
-    // then construct the thing
-    const newItems = {};
-    data.forEach((el: any) => {
+  useEffect(() => {
+    const r = {};
+    data.forEach((el) => {
       const f = el[0], i = el[1], n = el[2], s = '';
-      if (!(f in newItems)) {
-        newItems[f] = [];
+      if (!r[f]) {
+        r[f] = [];
       }
-      newItems[f].push({ i, n, s });
+
+      r[f].push({ i, n, s });
     });
 
-    setItems(newItems);
-  }
+    setItems(r);
+  }, [data]);
+
+  const [noFiles, setNoFiles] = useState(10);
 
   const onSelectImage = (fileName, imageIdx) => {
     const newItems = { ...items };
@@ -137,58 +91,10 @@ function ImportOld(props) {
 
   return (
     <>
-      <div style={{ backgroundColor: '#f0f0f0', borderBottom: '1px solid #000', position: 'fixed', width: '100%', marginTop: '-6em' }}>
-        <span className='float-left p-1'>
-          <span>Select XLSX file </span>
-          <input type="file" onChange={onFileUploaded}></input>
-          {
-            (sheetNames.length > 0) && (
-              <>
-                <span>Worksheet </span>
-                <select onChange={onSheetNameSelected}>
-                  <option value="">- select sheet name -</option>
-                  {sheetNames.map((el: any, idx) => {
-                    return (
-                      <option key={idx} value={el}>{el}</option>
-                    )
-                  })}
-                </select>
-                <span className='pl-2'>
-                  Showing {noFiles}/{Object.keys(items).length}
-                </span>
-              </>
-            )}
-        </span>
-        <span className='float-right p-1'>
-          {
-            (sheetName !== '') && (
-              <button onClick={() => exportStuff()} style={{
-                fontWeight: 'bolder',
-                padding: '0.5em 2em',
-                backgroundColor: 'green',
-                border: '1px solid green',
-                color: 'white',
-                cursor: 'pointer'
-              }}>Save</button>
-            )
-          }
-        </span>
-        <div style={{ clear: 'both' }}></div>
-      </div>
-      <div style={{ height: '4em' }}></div>
-      {/* <table className='table-example'>
-        <thead>
-          <tr>
-            <th>Org_Invoice_URL</th>
-            <th>Page_Image_URL</th>
-            <th>Page_Number</th>
-          </tr>
-        </thead>
-      </table> */}
       {
         Object.keys(items).map((k, idx) => {
           if (idx > noFiles) {
-            return <></>;
+            return null;
           }
 
           return (
@@ -196,23 +102,94 @@ function ImportOld(props) {
           );
         })
       }
-      <div className='p-1'>
-        <button onClick={() => setNoFiles(noFiles + 10)} className='p-1'>Load 10 more files</button>
-      </div>
+      <div style={{ height: '4em', clear: 'both' }}>&nbsp;</div>
+      {(sheetName !== '') &&
+        <div className='p-3 fixed-bottom text-center' style={{ backgroundColor: '#f0f0f0', borderTop: '1px solid #000' }}>
+          <button onClick={() => setNoFiles(noFiles + 10)} className='p-1 w-25 btn btn-primary float-left'>Load 10 more files</button>
+          <span className='mx-auto'>Showing {noFiles}/{Object.keys(items).length}</span>
+          <button onClick={exportStuff} className='btn btn-success w-25 float-right'>Save</button>
+        </div>
+      }
     </>
   );
 }
 
-function Import(props) {
+function ImportNew(props) {
 
   const numberOfImages = 100;
 
+  const { data, fileName, sheetName } = props;
+
   const [items, setItems] = useState([{}]);
+  const [noFiles, setNoFiles] = useState(numberOfImages);
+
+  useEffect(() => {
+    const newItems = data.map((el: any) => {
+      const f = el[0], i = el[1], n = el[2], s = '';
+
+      return { f, i, n, s };
+    });
+
+    setItems(newItems);
+  }, [data]);
+
+  const onSelectImage = (imageIdx) => {
+    const newItems = [...items];
+    newItems[imageIdx]['s'] = Boolean(items[imageIdx]['s']) ? '' : 'selected';
+    setItems(newItems);
+  }
+
+  const exportStuff = () => {
+    const wb = XLSX.utils.book_new();
+    const dt = [['File', 'Image', 'PageNo', 'Status']];
+    items.forEach((o: any, idx) => {
+      dt.push([
+        o.f,
+        o.i,
+        o.n,
+        o.s !== '' ? o.s : (noFiles > idx ? 'viewed' : '')
+      ]);
+    });
+
+    const ws = XLSX.utils.aoa_to_sheet(dt);
+    XLSX.utils.book_append_sheet(wb, ws, 'Result', true);
+    XLSX.writeFileXLSX(wb, `Result - ${fileName} - ${sheetName} - ${new Date()}.xlsx`);
+  }
+
+  return (<>
+    {
+      items.map((el: any, idx) => {
+        if (idx > noFiles) {
+          return <></>;
+        }
+
+        return (
+          <Thumbnail key={idx} img={el.i} isSelected={Boolean(el.s)} onSelect={() => onSelectImage(idx)} />
+        );
+      })
+    }
+    <div style={{ height: '4em', clear: 'both' }}>&nbsp;</div>
+    {(sheetName !== '') &&
+      <>
+        <div className='p-3 fixed-bottom text-center' style={{ backgroundColor: '#f0f0f0', borderTop: '1px solid #000' }}>
+          <button onClick={() => setNoFiles(noFiles + numberOfImages)} className='p-1 w-25 btn btn-primary float-left'>Load {numberOfImages} more images</button>
+          <span className='center'>Showing {noFiles}/{Object.keys(items).length}</span>
+          <button onClick={exportStuff} className='btn btn-success w-25 float-right'>Save</button>
+        </div>
+      </>
+    }
+  </>
+  );
+}
+
+function Import(props) {
   const [sheetNames, setSheetNames] = useState<Array<String>>([]);
   const [sheetName, setSheetName] = useState('');
   const [wb, setWb] = useState(null);
   const [fileName, setFileName] = useState('');
-  const [noFiles, setNoFiles] = useState(numberOfImages);
+  const [data, setData] = useState<Array<any>>([]);
+  const [noFiles, setNoFiles] = useState(0);
+  const [importType, setImportType] = useState(0);
 
   const onFileUploaded = (e) => {
     const [file] = e.target.files;
@@ -238,9 +215,9 @@ function Import(props) {
     setSheetName(sheetNameX);
     const wbX: any = wb;
     const ws = wbX.Sheets[sheetNameX];
-    const data = XLSX.utils.sheet_to_json(ws, { header: 1 });// skip header
+    const dt = XLSX.utils.sheet_to_json(ws, { header: 1 });// skip header
     // then sort the data
-    data.sort((a: any, b: any) => {
+    dt.sort((a: any, b: any) => {
       if (a[0] === b[0]) {
         if (a[2] < b[2]) {
           return -1;
@@ -256,43 +233,36 @@ function Import(props) {
       return 1;
     });
 
-    // then construct the thing
-    const newItems = data.map((el: any) => {
-      const f = el[0], i = el[1], n = el[2], s = '';
-
-      return { f, i, n, s };
-    });
-
-    setItems(newItems);
+    setData(dt);
   }
 
-  const onSelectImage = (imageIdx) => {
-    const newItems = [...items];
-    newItems[imageIdx]['s'] = Boolean(items[imageIdx]['s']) ? '' : 'selected';
-    setItems(newItems);
-  }
+  // const onSelectImage = (imageIdx) => {
+  //   const newItems = [...items];
+  //   newItems[imageIdx]['s'] = Boolean(items[imageIdx]['s']) ? '' : 'selected';
+  //   setItems(newItems);
+  // }
 
-  const exportStuff = () => {
-    const wb = XLSX.utils.book_new();
-    const dt = [['File', 'Image', 'PageNo', 'Status']];
-    items.forEach((o: any, idx) => {
-      dt.push([
-        o.f,
-        o.i,
-        o.n,
-        o.s !== '' ? o.s : (noFiles > idx ? 'viewed' : '')
-      ]);
-    });
+  // const exportStuff = () => {
+  //   const wb = XLSX.utils.book_new();
+  //   const dt = [['File', 'Image', 'PageNo', 'Status']];
+  //   items.forEach((o: any, idx) => {
+  //     dt.push([
+  //       o.f,
+  //       o.i,
+  //       o.n,
+  //       o.s !== '' ? o.s : (noFiles > idx ? 'viewed' : '')
+  //     ]);
+  //   });
 
-    const ws = XLSX.utils.aoa_to_sheet(dt);
-    XLSX.utils.book_append_sheet(wb, ws, 'Result', true);
-    XLSX.writeFileXLSX(wb, `Result - ${fileName} - ${sheetName} - ${new Date()}.xlsx`);
-  }
+  //   const ws = XLSX.utils.aoa_to_sheet(dt);
+  //   XLSX.utils.book_append_sheet(wb, ws, 'Result', true);
+  //   XLSX.writeFileXLSX(wb, `Result - ${fileName} - ${sheetName} - ${new Date()}.xlsx`);
+  // }
 
   return (
-    <>
+    <div style={{ backgroundColor: '#ffffff' }}>
       <div style={{ backgroundColor: '#f0f0f0', borderBottom: '1px solid #000', position: 'fixed', width: '100%', marginTop: '-6em' }}>
-        <span className='float-left p-1'>
+        <span className='float-left p-3'>
           <span>Select XLSX file </span>
           <input type="file" onChange={onFileUploaded}></input>
           {
@@ -307,25 +277,15 @@ function Import(props) {
                     )
                   })}
                 </select>
-                <span className='pl-2'>
-                  Showing {noFiles}/{Object.keys(items).length}
-                </span>
               </>
             )}
         </span>
-        <span className='float-right p-1'>
-          {
-            (sheetName !== '') && (
-              <button onClick={() => exportStuff()} style={{
-                fontWeight: 'bolder',
-                padding: '0.5em 2em',
-                backgroundColor: 'green',
-                border: '1px solid green',
-                color: 'white',
-                cursor: 'pointer'
-              }}>Save</button>
-            )
-          }
+        <span className='float-right p-3'>
+          <span>View type </span>
+          <select onChange={(e) => setImportType(parseInt(e.target.value))}>
+            <option value="0">Group</option>
+            <option value="1">List</option>
+          </select>
         </span>
         <div style={{ clear: 'both' }}></div>
       </div>
@@ -339,21 +299,9 @@ function Import(props) {
           </tr>
         </thead>
       </table> */}
-      {
-        items.map((el: any, idx) => {
-          if (idx > noFiles) {
-            return <></>;
-          }
-
-          return (
-            <Thumbnail key={idx} img={el.i} isSelected={Boolean(el.s)} onSelect={() => onSelectImage(idx)} />
-          );
-        })
-      }
-      <div className='p-1' style={{ clear: 'both' }}>
-        <button onClick={() => setNoFiles(noFiles + numberOfImages)} className='p-2 w-100' style={{ border: '2px solid green' }} >Load {numberOfImages} of images</button>
-      </div>
-    </>
+      {(importType === 0) && <ImportOld data={data} sheetName={sheetName} fileName={fileName} />}
+      {(importType === 1) && <ImportNew data={data} sheetName={sheetName} fileName={fileName} />}
+    </div>
   );
 }
 
