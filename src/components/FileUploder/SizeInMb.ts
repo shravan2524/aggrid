39dca09adaaa3ec912967d3a30baf00bbde3f9b4
irenemmassy <laummassy@gmail.com>
2022-06-09 10:51:56 +0300

    updates

--- src/components/FileUploder/SizeInMb.ts
+++ src/components/FileUploder/SizeInMb.ts
@@ -1,13 +1,1 @@
 export const SizeInMB = (bytes: number): string => `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
-export const accepted = () => {
-  const accept = {
-    'application/json': [],
-    'application/vnd.ms-excel': [],
-    'application/vnd.ms-powerpoint': [],
-    'application/pdf': [],
-    'application/msword': [],
-    'image/*': [],
-    'text/*': [],
-  };
-  return accept;
-};
