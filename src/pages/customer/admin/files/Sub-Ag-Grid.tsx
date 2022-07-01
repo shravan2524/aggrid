d711998a1aa38f757767db5b952e0850a8952103
irenemmassy <laummassy@gmail.com>
2022-07-01 10:52:26 +0300

    fix filter

--- src/pages/customer/admin/files/Sub-Ag-Grid.tsx
+++ src/pages/customer/admin/files/Sub-Ag-Grid.tsx
@@ -8,33 +8,6 @@ import React, {
 import { AgGridReact } from 'ag-grid-react';
 import { ColDef, ICellRendererParams } from 'ag-grid-community';
 import { fetchFileContentData } from 'services/filesAPIService';
-import { ColumnsDates, ColumnsNumber } from './Datas';
-
-// Filter Date
-const filterParams = {
-  comparator: (filterLocalDateAtMidnight: Date, cellValue: string) => {
-    const dateAsString = cellValue;
-    if (dateAsString == null) return -1;
-    const dateParts = dateAsString.split('/');
-    const cellDate = new Date(
-      Number(dateParts[2]),
-      Number(dateParts[1]) - 1,
-      Number(dateParts[0]),
-    );
-    if (filterLocalDateAtMidnight.getTime() === cellDate.getTime()) {
-      return 0;
-    }
-    if (cellDate < filterLocalDateAtMidnight) {
-      return -1;
-    }
-    if (cellDate > filterLocalDateAtMidnight) {
-      return 1;
-    }
-
-    return dateAsString;
-  },
-  browserDatePicker: true,
-};
 
 // main Function
 export default function DetailCellRenderer({
@@ -49,13 +22,8 @@ export default function DetailCellRenderer({
   const Columns = data.agGridColumns.map((f: any) => ({
     headerName: f.columnTitle,
     field: f.columnName,
-    filter: ColumnsDates.includes(f.columnName)
-      ? 'agDateColumnFilter'
-      : ColumnsNumber.includes(f.columnName)
-        ? 'agNumberColumnFilter'
-        : 'agTextColumnFilter',
+    filter: 'agTextColumnFilter',
     editable: false,
-    filterParams: ColumnsDates.includes(f.columnName) && filterParams,
   }));
   // default Columns settings
   const [columnDefs, setColumnDefs] = useState(Columns);
