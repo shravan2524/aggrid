fd432f811ef9be23f04fefe43ae132062f16cc96
Rakesh Kumar Sahoo <34296104+webkitty10@users.noreply.github.com>
2022-08-11 14:46:13 +0530

    Update ReconciliationPrPage.tsx

--- src/pages/customer/reconciliation/pr/ReconciliationPrPage.tsx
+++ src/pages/customer/reconciliation/pr/ReconciliationPrPage.tsx
@@ -29,7 +29,7 @@ function CustomActionsToolPanel(onBtExport:any) {
 export default function ReconciliationPrPage() {
   const gridRef = useRef<any>();
   const { height } = useWindowDimensions();
-  const containerStyle = useMemo(() => ({ width: '100%', height: `${(height)}px`, minHeight: '600px' }), [height]);
+  const containerStyle = useMemo(() => ({ width: '100%', height: '77vh' }), [height]);
   const [rowData, setRowData] = useState<any>();
   const [columnDefs, setColumnDefs] = useState([{
     headerName: 'PR Details',
