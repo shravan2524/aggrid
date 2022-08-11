123b5f59a40b485d445ab3f4383c6b8498194064
Rakesh Kumar Sahoo <34296104+webkitty10@users.noreply.github.com>
2022-08-11 14:12:13 +0530

    Update BaseFooter.tsx

--- src/parts/footer/BaseFooter.tsx
+++ src/parts/footer/BaseFooter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function BaseFooter() {
   return (
-    <footer className="m-0 p-0 text-purple-dark text-center footer mt-auto py-3">
+    <footer className="m-0 p-0 text-purple-dark text-center footer mt-auto py-2">
       <footer className="d-flex flex-wrap justify-content-center align-items-center align-middle">
         <p className="col-md-4 mb-0 text-muted">© 2022 Finkraft</p>
       </footer>
